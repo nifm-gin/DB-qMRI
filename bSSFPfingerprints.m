31715f690221aa3af49667fc9e8d90adddba0fc6
bouxfa <boux.fabien@gmail.com>
2020-01-16 18:00:32 +0100

    Finished major modifications

--- bSSFPfingerprints.m
+++ bSSFPfingerprints.m
@@ -1,9 +1,8 @@
 
 %% Description
 %
-% We want to evaluate the DBL method with usual MRF fingerprints. This
-% experiment is based on the one used to investigate the impact of
-% dictionary size. 
+% We evaluate the DBL method with usual MRF fingerprints. This experiment
+% is based on the one used to investigate the impact of dictionary size. 
 %
 % Fabien Boux - 01/2020
 
@@ -20,8 +19,8 @@ int_T2  = 1e-3 * [20  3020];    % between 20 and 3000 ms
 int_df  = 2*pi * [-400 400];  	% +/- 400 Hz
 
 nb_param = 2;   % 2 for T1 and T2 estimates, and 3 for T1, T2 and Df estimates
-nb_signals = [400 1600 6400]; % if nb_param == 2
-% nb_signals = [1000 3375 8000]; % if nb_param == 3
+nb_signals = [400 3600]; % if nb_param == 2
+% nb_signals = [1000 8000]; % if nb_param == 3
     
 % Simulation settings
 FA      = (pi/180)* [90 ...
@@ -34,7 +33,7 @@ TR      = (TR - min(TR)) ./ max(TR - min(TR));
 TR      = 1e-3 * (10.5  + 3.5 * TR);
 
 % Experiment settings
-snr_levels = [logspace(1, 2.035, 19) inf];
+snr_levels = [logspace(1, 2.035, 39) inf];
 nb_test_signals = 10000;
 
 % Regression settings
@@ -52,15 +51,16 @@ snr_train = 60;
 addpath(genpath('functions'))
 addpath(genpath('tools'))
 
-
-%% Processing 
-
+% Init
 t_grid          = nan(length(snr_levels), length(nb_signals));
 t_gllim         = t_grid;
 mNRMSE_grid     = nan(length(snr_levels), length(nb_signals), nb_param);
 mRMSE_grid      = mNRMSE_grid; mMAE_grid = mNRMSE_grid;
 mNRMSE_gllim    = mNRMSE_grid;
 mRMSE_gllim     = mNRMSE_gllim; mMAE_gllim = mNRMSE_gllim; 
+
+
+%% Processing 
 
 if verbose >= 1, disp('Dictionary size'); end
 
@@ -71,9 +71,9 @@ for f = 1:size(nb_signals,2)
     % Compute dico grid
     clear X Y  
     nb_step = floor(nb_signals(f)^(1/nb_param));
-    v1  = int_T1(1) : (int_T1(2) - int_T1(1)) / nb_step : int_T1(2);
-    v2  = int_T2(1) : (int_T2(2) - int_T2(1)) / nb_step : int_T2(2);
-    v3  = int_df(1) : (int_df(2) - int_df(1)) / nb_step : int_df(2);
+    v1  = int_T1(1) : (int_T1(2) - int_T1(1)) / (nb_step-1) : int_T1(2);
+    v2  = int_T2(1) : (int_T2(2) - int_T2(1)) / (nb_step-1) : int_T2(2);
+    v3  = int_df(1) : (int_df(2) - int_df(1)) / (nb_step-1) : int_df(2);
     if nb_param == 2
         Y(:,1)  = repmat(v1, 1, length(v2));
         Y(:,2)  = repelem(v2, 1, length(v2));
@@ -96,7 +96,7 @@ for f = 1:size(nb_signals,2)
     if nb_param == 2
         D       = MRF_dictionary(Y(:,1), Y(:,2), [], FA, TR); 
     elseif nb_param == 3
-        Y(:,3) = int_df(1) + (int_df(2) - int_df(1)) * Y(:,3);
+        Y(:,3)  = int_df(1) + (int_df(2) - int_df(1)) * Y(:,3);
         D       = MRF_dictionary(Y(:,1), Y(:,2), Y(:,3), FA, TR); 
     end 
     X       = abs(D.magnetization');
@@ -109,14 +109,14 @@ for f = 1:size(nb_signals,2)
     end
     
     clear D
-    for snr = 1:length(snr_levels)
+    parfor snr = 1:length(snr_levels)
 
         if verbose == 2, disp(['f = ' num2str(f) ' Snr order: ' num2str(snr_levels(snr))]); end
 
         % Generate test data
-        Ytest       = rand(nb_test_signals,nb_param);
-        Ytest(:,1)  = int_T1(1) + (int_T1(2) - int_T1(1)) * Ytest(:,1);
-        Ytest(:,2)  = int_T2(1) + (int_T2(2) - int_T2(1)) * Ytest(:,2); 
+        Ytest 	= rand(nb_test_signals,nb_param);
+        Ytest(:,1) = int_T1(1) + (int_T1(2) - int_T1(1)) * Ytest(:,1);
+        Ytest(:,2) = int_T2(1) + (int_T2(2) - int_T2(1)) * Ytest(:,2); 
         if nb_param == 2
             D       = MRF_dictionary(Ytest(:,1), Ytest(:,2), [], FA, TR); 
         elseif nb_param == 3
@@ -159,7 +159,7 @@ mRMSE(:,2,:,:)  = mRMSE_gllim;
 mMAE(:,1,:,:)   = mMAE_grid;
 mMAE(:,2,:,:)   = mMAE_gllim;
 
-fing_signals = X(randi(size(X,1),10,1),:);
+fing_signals    = X(randi(size(X,1),10,1),:);
 
 
 %% Saving 
@@ -181,104 +181,65 @@ colors  = [          0    0.4470    0.7410
                 0.6350    0.0780    0.1840];
 colors = [colors; colors];
     
-fig_supp = figure;
+fig = figure;
 
-subplot(2,2,1)
+subplot(4,4,[1 2])
 plot(FA * 180 / pi)
 xlabel('TR index'); ylabel('FA (degrees)')
 ylim([0 90]); xlim([0 length(TR)])
 
-subplot(2,2,3)
+subplot(4,4,[5 6])
 plot(TR*1e3)
 xlabel('TR index'); ylabel('TR (ms)')
 ylim([10 14]); xlim([0 length(TR)])
 
-subplot(2,2,[2 4])
+subplot(4,4,[3 4 7 8])
 plot(fing_signals')
 ylim([0 .2]); xlim([0 length(TR)])
+xlabel('TR index');
+title('Signals of dictionary')
 
-
-%%
-    
-fig = figure;
 
 count = 1;
 for f = 1:length(nb_signals)
 
-    if nb_signals(f) >= 100 % If we have at least 100 signals 
+    ax(count) = subplot(4,4,[9 10 13 14]+2*(f-1));
+    set(groot,'defaultAxesColorOrder',colors)
 
-        ax(count) = subplot(nb_param,length(nb_signals),f);
-        set(groot,'defaultAxesColorOrder',colors)
+    hold on;
+%     plot([0 200], [mRMSE(end,1,f,1) mRMSE(end,1,f,1)], '--', 'linewidth',1.5, 'Color',colors(1,:),'HandleVisibility','off')        
+%     plot([0 200], [mRMSE(end,1,f,2) mRMSE(end,1,f,2)], '--', 'linewidth',1.5, 'Color',colors(2,:),'HandleVisibility','off')        
 
-        hold on;
-        plot([0 200], [mRMSE(end,1,f,1) mRMSE(end,1,f,1)], '--', 'linewidth',1.5, 'Color',colors(1,:),'HandleVisibility','off')        
-        plot([0 200], [mRMSE(end,2,f,1) mRMSE(end,2,f,1)], '--', 'linewidth',1.5, 'Color',colors(2,:),'HandleVisibility','off')        
+    plot(real_snr(:,f), squeeze(mRMSE(:,1,f,1)), '*-', 'MarkerSize', 12, 'Color', colors(1,:))
+    plot(real_snr(:,f), squeeze(mRMSE(:,1,f,2)), '*-', 'MarkerSize', 12, 'Color', colors(2,:))
 
-        plot(real_snr(:,f), mRMSE(:,1,f,1), '.-', 'MarkerSize', 12, 'Color', colors(1,:))
-        plot(real_snr(:,f), mRMSE(:,2,f,1), '.-', 'MarkerSize', 12, 'Color', colors(2,:))
+%     plot([0 200], [mRMSE(end,2,f,1) mRMSE(end,2,f,1)], '--', 'linewidth',1.5, 'Color',colors(1,:),'HandleVisibility','off')        
+%     plot([0 200], [mRMSE(end,2,f,2) mRMSE(end,2,f,2)], '--', 'linewidth',1.5, 'Color',colors(2,:),'HandleVisibility','off')        
 
-        if f ==  size(nb_signals,2), legend('DBM','DBL'); end
-        title([num2str(nb_signals(f)) ' signals']);
-        if count == 1, ylabel('T_1 RMSE (s)'); end
-        
-        axis tight;
-        set(gca,'FontSize',12)
-        lgd.FontSize = 16;
-        
-        xlim([5 110])
-        
-        ax(length(nb_signals)+count) = subplot(nb_param,length(nb_signals),length(nb_signals)+f);
-        set(groot,'defaultAxesColorOrder',colors)
+    plot(real_snr(:,f), squeeze(mRMSE(:,2,f,1)), '.-', 'MarkerSize', 12, 'Color', colors(1,:))
+    plot(real_snr(:,f), squeeze(mRMSE(:,2,f,2)), '.-', 'MarkerSize', 12, 'Color', colors(2,:))
+    
+    if f ==  size(nb_signals,2), legend('T_1 - DBM','T_2 - DBM', 'T_1 - DBL','T_2 - DBL'); end
+    title([num2str(nb_signals(f)) ' signals']);
+    if count == 1, ylabel('RMSE (s)'); end
 
-        hold on;
-        plot([0 200], [mRMSE(end,1,f,2) mRMSE(end,1,f,2)], '--', 'linewidth',1.5, 'Color',colors(1,:),'HandleVisibility','off')        
-        plot([0 200], [mRMSE(end,2,f,2) mRMSE(end,2,f,2)], '--', 'linewidth',1.5, 'Color',colors(2,:),'HandleVisibility','off')        
+    axis tight;
+    set(gca,'FontSize',12)
+    lgd.FontSize = 16;
 
-        plot(real_snr(:,f), mRMSE(:,1,f,2), '.-', 'MarkerSize', 12, 'Color', colors(1,:))
-        plot(real_snr(:,f), mRMSE(:,2,f,2), '.-', 'MarkerSize', 12, 'Color', colors(2,:))
-        
-        if count == 1, ylabel('T_2 RMSE (s)'); end
-        
-        axis tight;
-        set(gca,'FontSize',12)
-        lgd.FontSize = 16;
-        
-        xlim([5 110])
-        
-        if nb_param == 3
-            ax(length(nb_signals)+count) = subplot(nb_param,length(nb_signals),2*length(nb_signals)+f);
-            set(groot,'defaultAxesColorOrder',colors)
-
-            hold on;
-            plot([0 200], [mRMSE(end,1,f,3) mRMSE(end,1,f,3)], '--', 'linewidth',1.5, 'Color',colors(1,:),'HandleVisibility','off')        
-            plot([0 200], [mRMSE(end,2,f,3) mRMSE(end,2,f,3)], '--', 'linewidth',1.5, 'Color',colors(2,:),'HandleVisibility','off')        
-
-            plot(real_snr(:,f), mRMSE(:,1,f,3), '.-', 'MarkerSize', 12, 'Color', colors(1,:))
-            plot(real_snr(:,f), mRMSE(:,2,f,3), '.-', 'MarkerSize', 12, 'Color', colors(2,:))
-
-            if count == 1, ylabel('Df'); end
-            
-            axis tight;
-            set(gca,'FontSize',12)
-            lgd.FontSize = 16;
-
-            xlim([5 110])
-        end
-        
-        count = count +1;
-    end
+    xlim([5 110])
+    ylim([0.01 0.5])
 
     xlabel('SNR');
 end
 
-linkaxes(ax(1:length(nb_signals)),'xy')
-linkaxes(ax(length(nb_signals)+1:nb_param*length(nb_signals)),'xy')
+% linkaxes(ax(1:length(nb_signals)),'xy')
+% linkaxes(ax(length(nb_signals)+1:nb_param*length(nb_signals)),'xy')
 
 
 %% Exporting figures
 
 if backup == 1
-    savefig(fig, ['figures/' 'bSSFPfingerprints'])
-    savefig(fig_supp, ['figures/' 'bSSFPfingerprints-supp'])
+    savefig(fig, ['figures/' 'bSSFPfingerprints-supp'])
 end
 
