53770decad11fb0825b22cef674580db6da7cbda
bouxfa <boux.fabien@gmail.com>
2020-02-04 11:40:00 +0100

    Replaced the old strategy to correct CI values

--- ConfidenceIndex.m
+++ ConfidenceIndex.m
@@ -2,10 +2,9 @@
 %% Description
 %
 % We investigate the correlation between the confidence index (CI) and the
-% estimate errors (RMSE). Two experiments follow each other to produce an
-% efficient process. 
+% estimate errors (RMSE).
 %
-% Fabien Boux - 01/2020
+% Fabien Boux - 02/2020
 
 
 %% Setting
@@ -16,17 +15,14 @@ backup  = 1;
 
 % Signal settings
 int   	= [0.1 1];
-nb_param = 2;
-nb_train_signals = 2000;
+nb_param = 3;
+nb_train_signals = 10000;
 nb_test_signals  = 10000;
-snr_train_p1 = 40;
-snr_test_p1 = [20 40 60];
-snr_train_p2 = [20 30 40 50 60 70 80];
-snr_test_p2 = logspace(1, 2, 20);
+snr_train = 60;
+snr_test = [20 30 40 60 100];
 
 % Experiment settings
-nb_repetition_p1 = 100;
-nb_repetition_p2 = 25;
+nb_repetition = 100;
 
 % Regression settings
 Parameters.K = 50;
@@ -43,17 +39,15 @@ addpath(genpath('functions'))
 addpath(genpath('tools'))
 
 % Init
-pp_std = nan(length(snr_test_p1),nb_param,nb_repetition_p1);
-pp_err = pp_std;
+pp_std = nan(length(snr_test),nb_param,nb_repetition);
+pp_err = pp_std; pp_err2 = pp_std; pp_std2 = pp_err2;
 
 
-%% Processing 1
+%% Processing
 
-disp('First process')
-
-parfor rep = 1:nb_repetition_p1
+parfor rep = 1:nb_repetition
     
-    if verbose == 1, disp([num2str(rep) '/' num2str(nb_repetition_p1)]); end
+    if verbose == 1, disp([num2str(rep) '/' num2str(nb_repetition)]); end
     
     p   = [.01 .01];
     while min(abs(pdist(p',@(x,y) x-y))) < .1
@@ -76,16 +70,18 @@ parfor rep = 1:nb_repetition_p1
 
     % GlliM learning
     Dico = [];
-    Dico{1}.MRSignals       = AddNoise(Xtrain, snr_train_p1);
+    [Dico{1}.MRSignals, real_snr_train] = AddNoise(Xtrain, snr_train);
     Dico{1}.Parameters.Par  = Ytrain;
     [~,Params]  = AnalyzeMRImages(Xtrain,Dico,'DBL',Parameters);
     
     % Estimates
     tmp_pp_std = [];
     tmp_pp_err = [];
-    for s = 1:length(snr_test_p1)
+    tmp_pp_std2 = [];
+    tmp_pp_err2 = [];
+    for s = 1:length(snr_test)
         
-        Xtest_noisy = AddNoise(Xtest, snr_test_p1(s));
+        [Xtest_noisy, real_snr] = AddNoise(Xtest, snr_test(s));
         
         Estim   = AnalyzeMRImages(Xtest_noisy, [], 'DBL', Params);
         Ygllim  = squeeze(Estim.Regression.Y(:,1:nb_param));
@@ -94,112 +90,65 @@ parfor rep = 1:nb_repetition_p1
 
         tmp_pp_std(s,:) = nanmean(Cov,1).^.5;
         tmp_pp_err(s,:) = Rmse;
+        
+        Params_updt = Params;
+        var_noise = abs( mean((max(Xtest,[],2) ./ real_snr).^2) ); ...
+%             - mean((max(Dico{1}.MRSignals,[],2) ./ real_snr_train).^2) );
+%         var_noise = 1 ./ mean(real_snr).^2;
+        Params_updt.theta = updateSigma(Params.theta, var_noise);
+        
+        Estim   = AnalyzeMRImages(Xtest_noisy, [], 'DBL', Params_updt);
+        Ygllim  = squeeze(Estim.Regression.Y(:,1:nb_param));
+        Cov     = squeeze(Estim.Regression.Cov);
+        Rmse    = EvaluateEstimation(Ytest, Ygllim);
+
+        tmp_pp_std2(s,:) = nanmean(Cov,1).^.5;
+        tmp_pp_err2(s,:) = Rmse;
     end
     
     pp_std(:,:,rep) = tmp_pp_std;
     pp_err(:,:,rep) = tmp_pp_err;
+    
+    pp_std2(:,:,rep) = tmp_pp_std2;
+    pp_err2(:,:,rep) = tmp_pp_err2;
 end
 
-%% Saving (process 1)
+
+%% Saving
 
 if backup == 1
     clear tmp* X* Y* Dico* Estim
-    save(['temp/' 'ConfidenceIndex-1'])
-end
-
-
-%% Processing 2
-
-disp('Second process')
-
-cc  = nan(nb_param, length(snr_test_p2), nb_repetition_p2, length(snr_train_p2));
-
-for st = 1:length(snr_train_p2)
-    
-    tmp_pp_std = nan(nb_param, length(snr_test_p2));
-    tmp_pp_err = tmp_pp_std;
-
-    if verbose == 1, disp(['SNR_train ' num2str(st) '/' num2str(length(snr_train_p2))]); end
-    
-    parfor rep = 1:nb_repetition_p2
-
-        if verbose > 1, disp([num2str(rep) '/' num2str(nb_repetition_p2)]); end
-
-        p   = [.01 .01];
-        while min(abs(pdist(p',@(x,y) x-y))) < .1
-            p   = 0.1 + 0.9*rand(1,5);
-        end
-
-        % Generate training signals
-        Xtrain  = [];
-        Ytrain	= int(1) + (int(2)-int(1)) * net(scramble(sobolset(nb_param),'MatousekAffineOwen'),nb_train_signals);
-        for sim = 1:size(Ytrain,1)
-            Xtrain(sim,:) = toyMRsignal(Ytrain(sim,:),p(1:nb_param));
-        end
-        [Xtrain,snrtmp] = AddNoise(Xtrain, snr_train_p2(st));
-        real_snr_train(:,rep) = nanmean(snrtmp);
-
-        % Generate test signals
-        Xtest   = [];
-        Ytest	= int(1) + (int(2)-int(1)) * rand(nb_test_signals,nb_param);
-        for sim = 1:size(Ytest,1)
-            Xtest(sim,:) = toyMRsignal(Ytest(sim,:),p(1:nb_param));
-        end
-
-        % GlliM learning
-        Dico    = [];
-        Dico{1}.MRSignals       = Xtrain;
-        Dico{1}.Parameters.Par  = Ytrain;
-        [~,Params]  = AnalyzeMRImages(Xtrain,Dico,'DBL',Parameters);
-
-        % Estimating
-        tmp_pp_std = [];
-        tmp_pp_err = [];
-        for s = 1:length(snr_test_p2)
-
-            Xtest_noisy = AddNoise(Xtest, snr_test_p2(s));
-
-            Estim   = AnalyzeMRImages(Xtest_noisy, [], 'DBL', Params);
-            Ygllim  = squeeze(Estim.Regression.Y(:,1:nb_param));
-            Cov     = squeeze(Estim.Regression.Cov);
-            Rmse    = EvaluateEstimation(Ytest, Ygllim);
-
-            tmp_pp_std(:,s) = nanmean(Cov,1).^.5;
-            tmp_pp_err(:,s) = Rmse;
-        end
-
-        cc(:,:,rep,st) = tmp_pp_std ./ tmp_pp_err;
-    end
-end
-
-
-%% Saving (process 2)
-
-if backup == 1
-    clear tmp* X* Y* Dico* Estim
-    save(['temp/' 'ConfidenceIndex-2'])
+    save(['temp/' 'ConfidenceIndex'])
 end
 
 
 %% Displaying
 
+colors  = [          0    0.4470    0.7410
+                0.8500    0.3250    0.0980
+                0.9290    0.6940    0.1250
+                0.4940    0.1840    0.5560
+                0.4660    0.6740    0.1880
+                0.3010    0.7450    0.9330
+                0.6350    0.0780    0.1840];
+colors = [colors; colors];
+
+
 fig = figure;
 
 param = 1; % first or second parameter lead to ~ same results
 
-subplot(121)
+subplot(131)
 hold on
 
 for f = 1:size(pp_std,1)
-    
-%     plot(squeeze(mean(pp_std(f,:,:),2))', squeeze(mean(pp_err(f,:,:),2))','.','MarkerSize',12)
-    plot(squeeze(pp_std(f,param,:))', squeeze(pp_err(f,param,:))','.','MarkerSize',12)
+    plot(squeeze(pp_std(f,param,:))', squeeze(pp_err(f,param,:))', '.','MarkerSize',12, 'color',colors(f,:))
 
     mdl = fitlm(squeeze(pp_std(f,param,:)), squeeze(pp_err(f,param,:)),'Intercept',false);
-    plot([0 max(pp_std(:))],[0 max(pp_std(:))]*mdl.Coefficients.Estimate, 'k')
+    plot([0 max(pp_std(:))], [0 max(pp_std(:))]*mdl.Coefficients.Estimate, 'k')
 
-    leg{2*f-1}  = ['SNR_{test} = ' num2str(snr_test_p1(f))];
-    leg{2*f}    = ['\alpha = ' num2str(mdl.Coefficients.Estimate,3) '   (R^2 = ' num2str(mdl.Rsquared.Ordinary,2) ')'];
+    leg{2*f-1}  = ['SNR_{test} = ' num2str(snr_test(f))];
+    leg{2*f}    = ['\alpha = ' num2str(mdl.Coefficients.Estimate,3) '   (R^2 = ' num2str(mdl.Rsquared.Ordinary,3) ')'];
 end
 
 title('RMSE = \alpha x CI')
@@ -207,26 +156,36 @@ xlabel('CI'); ylabel('RMSE')
 legend(leg)
 
 
-subplot(122)
-hold on
 clear leg
+subplot(132)
+hold on
+for f = 1:size(pp_std2,1)
+    plot(squeeze(pp_std2(f,param,:))', squeeze(pp_err2(f,param,:))', '.','MarkerSize',12, 'color',colors(f,:))
 
-F = @(xdata,x)x(1)*exp(-x(2)./xdata) + x(3);
-
-for st = 1:length(snr_train_p2)
-    
-    ff = mean(1./squeeze(cc(:,:,:,st)),3);
-    plot(snr_test_p2, ff(param,:),'.-','Markersize',15)
-
-    a = levenbergmarquardt(F, snr_test_p2, ff(param,:), [max(ff(param,:)) -1 min(ff(param,:))]);
-    plot(snr_test_p2, F(snr_test_p2,a), 'k--','HandleVisibility','off')
-
-    leg{st} = ['SNR_{train} = ' num2str(snr_train_p2(st))];
+    leg{f}  = ['SNR_{test} = ' num2str(snr_test(f))];
 end
-% plot([10 100], [1 1], 'k')
+mdl = fitlm(reshape(pp_std2(:,param,:),1,[]), reshape(pp_err2(:,param,:),1,[]),'Intercept',false);
+plot([0 max(pp_std2(:))], [0 max(pp_std2(:))]*mdl.Coefficients.Estimate, 'k--')
+leg{f+1} = ['\alpha = ' num2str(mdl.Coefficients.Estimate,3) '   (R^2 = ' num2str(mdl.Rsquared.Ordinary,3) ')'];
 
-title('\alpha = f(SNR_{test})')
-xlabel('SNR_{test}'); ylabel('\alpha')
+title('RMSE_{corrected} = \alpha x CI_{corrected}')
+xlabel('CI corrected'); ylabel('RMSE with Sigma corrected')
+legend(leg)
+
+
+clear leg
+subplot(133)
+hold on
+for f = 1:size(pp_std2,1)
+    plot(squeeze(pp_err(f,param,:))', squeeze(pp_err2(f,param,:))', '.','MarkerSize',12, 'color',colors(f,:))
+    
+    leg{f}  = ['SNR_{test} = ' num2str(snr_test(f))];
+end
+plot([0 max(pp_err(:))], [0 max(pp_err(:))], 'k')
+leg{f+1} = 'RMSE_{corrected} = RMSE';
+
+title('RMSE_{corrected} vs. RMSE')
+xlabel('RMSE'); ylabel('RMSE with Sigma corrected')
 legend(leg)
 
 
@@ -235,6 +194,3 @@ legend(leg)
 if backup == 1
     savefig(fig, ['figures/' 'ConfidenceIndex'])
 end
-
-
-
